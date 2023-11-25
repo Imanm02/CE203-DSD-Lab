a9edd3932cc3625c5e1f53b9b78c54e89254593d
Imanm02 <imanm1381@gmail.com>
2023-11-25 21:02:50 +0330

    update Practice 3
    Co-authored-by: Iman Mohammadi <imanm1381@gmail.com>
    Co-authored-by: Shayan Salehi <s.salehi1381@gmail.com>
    Co-authored-by: Amirmahdi Kousheshi <amk_amir82@yahoo.com>

--- Practices/Practice 3/Code/Part 2/sequential_comparator.v
+++ Practices/Practice 3/Code/Part 2/sequential_comparator.v
@@ -7,12 +7,15 @@ module sequential_comparator (
     output o_lt
 );
     wire o_gt_not, o_lt_not, i_gt, i_lt;
+    
     // Create the flip flops
     assign o_gt = ~(o_gt_not & ~(i_gt & clk));
     assign o_gt_not = ~(o_gt & ~(~i_gt & clk));
     assign o_lt = ~(o_lt_not & ~(i_lt & clk));
     assign o_lt_not = ~(o_lt & ~(~i_lt & clk));
+
     // Assign the inputs of flop flops
     assign i_gt = (~reset) & (o_gt | ((~i_lt) & (x > y)));
     assign i_lt = (~reset) & (o_lt | ((~i_gt) & (x < y)));
+
 endmodule 
